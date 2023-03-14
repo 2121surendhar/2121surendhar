5a22025591cc426f944ab1027b801558b249c0ee
king <villan@21>
2023-03-14 23:15:04 +0530

    Initial commit

---
+++ feb.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+ class remo {
+
+    public static int juli(int a, int b) {
+        return a + b;
+
+
+
+    }
+
+}
+public class feb {
+
+    public static void main(String[] args) {
+        remo god = new remo();
+
+        System.out.println(remo.juli(1,3));
+      int a=10;
+      if(a==11){
+          System.out.println("vignesh is the play boy");
+
+      }
+      else{
+      }
+
+
+
+    }
+}
+
+
+
+
+
+
